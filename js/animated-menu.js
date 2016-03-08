50661a25bba7a70b05878adb6b1020bbe8d76441
Isayur <azphel@yahoo.com>
2016-03-08 20:18:00 +0100

    Bugfixes
    Still need to fix menu not being on top of index slideshow

--- js/animated-menu.js
+++ js/animated-menu.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	
-	$(".js-dropdown").mouseenter(function(){
+	$("li.js-dropdown").mouseenter(function(){
 		$(this).stop().animate({height:'200px'},{speed:600})
 	});
 	
-	$(".js-dropdown").mouseleave(function(){
+	$("li.js-dropdown").mouseleave(function(){
 		$(this).stop().animate({height:'40px'},{speed:"fast"})
 	});
 	
