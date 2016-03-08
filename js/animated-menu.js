0e491e6d23fd51d05b2c33d0c05cd3ad1a4a259d
Isayur <azphel@yahoo.com>
2016-03-08 18:07:10 +0100

    Finalizing JS
    (Hopefully)

--- js/animated-menu.js
+++ js/animated-menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 	$(".js-dropdown").mouseenter(function(){
-		$(this).stop().animate({height:'150px'},{speed:600})
+		$(this).stop().animate({height:'200px'},{speed:600})
 	});
 	
 	$(".js-dropdown").mouseleave(function(){
