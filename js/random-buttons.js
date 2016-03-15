9af90bb9426a5d19f8fe08c0d78d6226cf2457ac
yasengeorgiev <yasen_96@abv.bg>
2016-03-15 14:32:28 +0100

    Random buttons DONE !!!!!!!!!!!

--- js/random-buttons.js
+++ js/random-buttons.js
@@ -1,18 +1,68 @@
+    var randomLink = function () {
 
-function loadExternalHTMLPage() {
-var xmlhttp;
-var pagesToDisplay = ['childhood.html', 'personal.html', 'philanthropy.html'];
-if (window.XMLHttpRequest) {
-xmlhttp = new XMLHttpRequest();
-} else {
-xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.onreadystatechange = function () {
-if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-document.getElementByClassName("nav-tweet-side").innerHTML = xmlhttp.responseText;
-}
-}
-var randomnumber = Math.floor(Math.random() * pagesToDisplay.length);
-xmlhttp.open("GET", pagesToDisplay[randomnumber], true);
-xmlhttp.send();
-}
+        // first create an array of links
+        var links = [
+			"childhood.html",
+            "personal.html",
+            "philanthropy.html",
+        ];
+
+        // then work out the maximum random number size
+        // by counting the number of links in the array
+        var max = (links.length)
+
+        // now generate a random number
+        var randomNumber = Math.floor(Math.random()*max);
+
+        // use that random number to retrieve a link from the array
+        var link = links[randomNumber];
+
+        // change the location of the window object
+        window.location =  link;
+    }
+	
+	var randomLink1 = function () {
+
+        // first create an array of links
+        var links1 = [
+            "tesla.html",
+            "spacex.html",
+			"solarcity.html",
+			"hyperloop.html"
+        ];
+
+        // then work out the maximum random number size
+        // by counting the number of links in the array
+        var max1 = (links1.length)
+
+        // now generate a random number
+        var randomNumber1 = Math.floor(Math.random()*max1);
+
+        // use that random number to retrieve a link from the array
+        var link1 = links1[randomNumber1];
+
+        // change the location of the window object
+        window.location =  link1;
+    }
+	
+	var randomLink2 = function () {
+
+        // first create an array of links
+        var links2 = [
+            "ai.html",
+            "extraterrestrial.html",
+        ];
+
+        // then work out the maximum random number size
+        // by counting the number of links in the array
+        var max2 = (links2.length)
+
+        // now generate a random number
+        var randomNumber2 = Math.floor(Math.random()*max2);
+
+        // use that random number to retrieve a link from the array
+        var link2 = links2[randomNumber2];
+
+        // change the location of the window object
+        window.location =  link2;
+    }
